22f068c6f9e335868e89bea6a51f7dde54959040
Andrei Toma <toma.andrei90@yahoo.com>
2020-05-30 16:23:06 +0300

    changed db url env variable

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,7 @@ const { checkTokenSetUser, isLoggedIn } = require('./auth/middlewares');
 const auth = require('./auth/index');
 
 mongoose.set('debug', process.env.NODE_ENV === 'production');
-mongoose.connect(process.env.DB_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
