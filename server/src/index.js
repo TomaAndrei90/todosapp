dbb1d8922c420116ca4b1e9f1b21511bdc247a82
Andrei Toma <toma.andrei90@yahoo.com>
2020-10-23 01:17:54 +0300

    debugging deploy refresh issue

--- server/src/index.js
+++ server/src/index.js
@@ -47,7 +47,7 @@ app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
 app.get('*', function(request, response) {
-  // response.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
+  response.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
